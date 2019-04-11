f92b8d0bc41bfaadc25ac5cd2318706fcda101be
SnekCode <code.snakes@gmail.com>
2019-04-11 09:04:15 -1000

    Front end logic built out!

---
+++ Acronym-front-end/apiCall.js
@@ -0,0 +1,29 @@
+const http = new XMLHttpRequest();
+const url = "http://tron.alohaspark.org/acronyms?bullets=";
+
+apiCall = function(bullets) {
+    http.open("GET", url + bullets);
+    http.send();
+    console.log("end of api call");
+};
+
+http.onreadystatechange = function(e){
+
+    let responseObject = JSON.parse(http.responseText);
+    let numAcronyms = responseObject.acronyms.length;
+    let textOutput = "";
+
+    
+
+    for (i=0;i<numAcronyms;i++) {
+        let tempDef = responseObject.acronyms[i].definition;
+        let tempName = responseObject.acronyms[i].name;
+        textOutput += tempDef + " (" + tempName + "); ";
+    };
+    textOutput=textOutput.substring(0,textOutput.length-2);
+
+    $("#outputText").val(textOutput);
+    console.log(http.responseText);
+}
+
+console.log("loaded");
